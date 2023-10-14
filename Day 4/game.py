1bda7d86f98b1881a6f1447778f2379581e99660
alex <a.n.chasovskoy@gmail.com>
2023-10-14 23:34:24 +0200

    Day 4 files

--- Day 4/game.py
+++ Day 4/game.py
@@ -2,23 +2,30 @@ from random import randint
 import emoji
 
 options = [":raised_fist:", ":victory_hand:", ":palm_down_hand:"]
-
-player_choice = int(input('Выбери: камень(1), ножницы(2) или бумага(3). \n'))
 computer_choice = randint(1, 3)
 
-if player_choice == computer_choice:
-    print(f"\nОба игрока выбрали {emoji.emojize(options[player_choice - 1])}.\nНичья!")
-elif ((player_choice == 1 and computer_choice == 2) or
-      (player_choice == 2 and computer_choice == 3) or
-      (player_choice == 3 and computer_choice == 1)):
-    print("\nИгрок выбрал:")
-    print(emoji.emojize(options[player_choice - 1]))
-    print(f"Компьютер выбрал:")
-    print(emoji.emojize(options[computer_choice - 1]))
-    print("Выиграл игрок. Поздравляю!!!")
-else:
-    print("\nИгрок выбрал:")
-    print(emoji.emojize(options[player_choice - 1]))
-    print(f"Компьютер выбрал:")
-    print(emoji.emojize(options[computer_choice - 1]))
-    print("Увы, в этот раз выиграл компьютер. Игрок проиграл...")
+try:
+    player_choice = int(input('Выбери: камень(1), ножницы(2) или бумага(3). \n'))
+
+    if player_choice > 3 or player_choice < 1:
+        print("Игрок ввёл неверный код. Компьютер выиграл...")
+    elif player_choice == computer_choice:
+        print(f"\nОба игрока выбрали {emoji.emojize(options[player_choice - 1])}.\nНичья!")
+    elif ((player_choice == 1 and computer_choice == 2) or
+          (player_choice == 2 and computer_choice == 3) or
+          (player_choice == 3 and computer_choice == 1)):
+        print("\nИгрок выбрал:")
+        print(emoji.emojize(options[player_choice - 1]))
+        print(f"Компьютер выбрал:")
+        print(emoji.emojize(options[computer_choice - 1]))
+        print("Выиграл игрок. Поздравляю!!!")
+    else:
+        print("\nИгрок выбрал:")
+        print(emoji.emojize(options[player_choice - 1]))
+        print(f"Компьютер выбрал:")
+        print(emoji.emojize(options[computer_choice - 1]))
+        print("Увы, в этот раз выиграл компьютер. Игрок проиграл...")
+except ValueError:
+    print("Игрок ввёл неверный код. Компьютер выиграл...")
+
+
