72059430177ec965cb7e678e0427dc5c53baa38e
alex <a.n.chasovskoy@gmail.com>
2023-10-16 14:26:26 +0200

    Day 7 files

--- Day 5/password_generator.py
+++ Day 5/password_generator.py
@@ -1,59 +1,59 @@
 import string
 from random import choice, shuffle
 
-lower = list(string.ascii_lowercase)
-upper = list(string.ascii_uppercase)
-numbers = list(string.digits)
-symbols = list(string.punctuation)
+# sets of symbols to use
+lower = set(string.ascii_lowercase)
+upper = set(string.ascii_uppercase)
+numbers = set(string.digits)
+symbols = set(string.punctuation)
 
 
-def random_symbol_choice(list_of_symbols, number_of_symbols):
-    random_symbols = []
-    if number_of_symbols > 0:
-        for x in range(number_of_symbols):
-            random_symbols.append(choice(list_of_symbols))
-        return random_symbols
-    else:
-        return random_symbols
+def random_symbol_choice(set_of_symbols, num):
+    """
+    A function to select certain number of random elements from a collection of symbols.
 
-
-def symbol_cleaning(initial_list, restricted_symbols_list):
-    if len(restricted_symbols_list) > 0:
-        for symbol in restricted_symbols_list:
-            if symbol in initial_list:
-                initial_list.remove(symbol)
-    return initial_list
+    :param set_of_symbols: initial list of symbols to make a choice from
+    :param num: number of symbols to be chosen
+    :return: a list of randomly chosen symbols
+    """
+    return [choice(list(set_of_symbols)) for _ in range(num)]
 
 
 try:
+    # user entered values
     length = abs(int(input('How many symbols do you want in your password?\n')))
-    upper_symbols = abs(int(input('How many upper case letters do you want in your password?\n')))
-    digits = abs(int(input('How many digits should be in your password?\n')))
-    num_symbols = abs(int(input('How many symbols do you want in your password?\n')))
-    restricted_symbols = input("Please enter restricted symbols separated by space:\n")
+    num_of_upper_symbols = abs(int(input('How many upper case letters do you want in your password?\n')))
+    num_of_digits = abs(int(input('How many digits should be in your password?\n')))
+    num_of_symbols = abs(int(input('How many symbols do you want in your password?\n')))
+    restricted_symbols = set(input('Please enter restricted symbols:\n'))
+    recommended_symbols = set(input('Please enter recommended symbols:\n'))
 
-    restricted_symbols = restricted_symbols.split(' ')
-    print(restricted_symbols)
+    # cleaning collections of the symbols if there are restricted symbols
+    if restricted_symbols and not recommended_symbols:
+        lower = lower - restricted_symbols
+        upper = upper - restricted_symbols
+        numbers = numbers - restricted_symbols
+        symbols = symbols - restricted_symbols
 
-    if len(restricted_symbols) > 0:
-        lower = symbol_cleaning(list(string.ascii_lowercase), restricted_symbols)
-        upper = symbol_cleaning(list(string.ascii_uppercase), restricted_symbols)
-        numbers = symbol_cleaning(list(string.digits), restricted_symbols)
-        symbols = symbol_cleaning(list(string.punctuation), restricted_symbols)
+    # if there are recommended symbols, the collection of symbols will be limited to recommendations
+    elif recommended_symbols:
+        symbols = recommended_symbols
 
-    if length > 0 and length > (upper_symbols + digits + num_symbols):
-        upper_list = random_symbol_choice(upper, upper_symbols)
-        lower_list = random_symbol_choice(lower, (length - upper_symbols - digits - num_symbols))
-        digit_list = random_symbol_choice(numbers, digits)
-        symbol_list = random_symbol_choice(symbols, num_symbols)
+    # randomly select required number of symbols from each collection
+    if length >= (num_of_upper_symbols + num_of_digits + num_of_symbols):
+        upper_list = random_symbol_choice(upper, num_of_upper_symbols)
+        lower_list = random_symbol_choice(lower, (length - num_of_upper_symbols - num_of_digits - num_of_symbols))
+        digit_list = random_symbol_choice(numbers, num_of_digits)
+        symbol_list = random_symbol_choice(symbols, num_of_symbols)
 
+    # collecting and randomizing the final password
         password_list = upper_list + lower_list + digit_list + symbol_list
         shuffle(password_list)
 
         password = ''.join(password_list)
         print(password)
     else:
-        print('Some numbers mismatch. Please review your conditions.')
+        print('Password length is not sufficient for the specified requirements.\nPlease review your conditions.')
 
 except ValueError:
-    print('You entered not a valid number. Try again.')
+    print('You did not enter a valid number. Please try again.')
---
+++ Day 7/hangman.py
@@ -0,0 +1,116 @@
+import string
+from random import choice
+
+lower = set(string.ascii_lowercase)
+word_list = ['beekeeper', 'dwarf', 'railway', 'notebook', 'mouse', 'house', 'nurse']
+counter = 0
+
+word = choice(word_list)
+
+substitute_list = ['_' for x in word]
+substitute = ' '.join(substitute_list)
+print(f'Word to guess is: {substitute}')
+game_over = False
+guessed_letters = set()
+
+
+def find_indices(my_word, character):
+    return [
+        index for index, item in enumerate(my_word)
+        if item == character
+    ]
+
+
+def letter_replacer(list_of_indexes, list_of_substitutes, letter):
+    for index in list_of_indexes:
+        list_of_substitutes[index] = letter
+    return ' '.join(substitute_list)
+
+
+def hangman_drawer(counter):
+    stages = [
+        '''
+           -----
+           |   |
+               |
+               |
+               |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+               |
+               |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+           |   |
+               |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+          /|   |
+               |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+          /|\\  |
+               |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+          /|\\  |
+          /    |
+        ''',
+        '''
+           -----
+           |   |
+           o   |
+          /|\\  |
+          / \\  |
+        '''
+    ]
+    print(stages[counter])
+
+
+while counter < 7:
+    if '_' in substitute:
+        letter = input('Guess a letter:\n').lower()
+
+        if letter not in lower:
+            print('This is not a valid letter')
+            hangman_drawer(counter)
+            counter += 1
+
+        elif letter in guessed_letters:
+            print('Do not repeat yourself.')
+            hangman_drawer(counter)
+            counter += 1
+
+        elif letter not in word:
+            print('You are wrong. This letter is not in our word.')
+            hangman_drawer(counter)
+            counter += 1
+
+        else:
+            indexes = find_indices(word, letter)
+            substitute = letter_replacer(indexes, substitute_list, letter=letter)
+            print(f"You are right.\n{substitute}")
+
+        guessed_letters.add(letter)
+
+    else:
+        print('You won!!! Congratulations!!!')
+        break
+
+if counter == 7:
+    print('You have lost. The poor man is dead (((')
