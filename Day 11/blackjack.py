adae52c5d17c98b2f2854c686a955713a35c96c7
alex <a.n.chasovskoy@gmail.com>
2023-10-20 21:09:22 +0200

    Day 11 files

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -4,68 +4,73 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 game_on = True
 
 
-def sum_check(player_cards_list, computer_cards_list):
-    if sum(computer_cards_list) < 17:
-        computer_cards_list.append(draw_card(cards))
-    elif sum(player_cards_list) == sum(computer_cards_list) == 21:
-        print('Draw!')
-        return continue_game()
-    elif sum(player_cards_list) == 21:
-        print('Blackjack! You win!')
-        return continue_game()
-    elif sum(player_cards_list) > 21:
-        print('You are over 21. You lose :-(')
-        return continue_game()
-    elif sum(computer_cards_list) == 21:
-        print(f'Computer has {sum(computer_cards_list)}. You lose :-(')
-        return continue_game()
-    else:
-        # If none of the above conditions are met, return False to indicate the game should not continue
-        return False
+def calculate_score(cards_list: list) -> int:
+    """
+    Function to calculate the score.
+    :param cards_list: List of the integers after the draw
+    :return: Total sum of the cards as an integer or 0 if player has Blackjack
+    """
+    score = sum(cards_list)
+    if len(cards_list) == 2 and score == 21:
+        return 0
+    elif score > 21 and 11 in cards_list:
+        cards_list = [1 if x == 11 else x for x in cards_list]
+    return sum(cards_list)
 
 
 def draw_card(cards_list: list) -> int:
+    """
+    Return random card from the deck.
+    :param cards_list: List with all cards available
+    :return: Card as integer
+    """
     return choice(cards_list)
 
 
-def initial_draw(card_list):
-    player_cards = [draw_card(cards_list=card_list), draw_card(cards_list=card_list)]
-    computer_cards = [draw_card(cards_list=card_list), draw_card(cards_list=card_list)]
-    results = [player_cards, computer_cards]
-    return results
-
-
-def continue_game():
-    check = input('Do you want to continue the game? "y" or "n"\n')
-    if check == 'y':
-        return True
-    elif check == 'n':
-        return False
+def compare_score(player_score, computer_score):
+    if player_score == computer_score:
+        return "Draw!"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack..."
+    elif player_score == 0:
+        return "Win with a Blackjack!"
+    elif player_score > 21:
+        return "You went over. You lose..."
+    elif computer_score > 21:
+        return "Opponent went over. You win!"
+    elif player_score > computer_score:
+        return "You win!"
     else:
-        print('Please check your answer.')
-        continue_game()
+        return "You lose..."
 
 
-initial_draw_cards = initial_draw(cards)
+player_cards = []
+computer_cards = []
 
-player_initial_draw = initial_draw_cards[0]
-computer_initial_draw = initial_draw_cards[1]
-print(f'Your cards are: {player_initial_draw}')
-print(f"Computer's first card is: {computer_initial_draw[0]}")
+for _ in range(2):
+    player_cards.append(draw_card(cards))
+    computer_cards.append(draw_card(cards))
+player_score = 0
+computer_score = 0
+
 while game_on:
-    game_on = sum_check(player_initial_draw, computer_initial_draw)
-    initial_draw_cards = initial_draw(cards)
+    player_score = calculate_score(player_cards)
+    computer_score = calculate_score(computer_cards)
 
-    player_initial_draw = initial_draw_cards[0]
-    computer_initial_draw = initial_draw_cards[1]
-    print(f'Your cards are: {player_initial_draw}')
-    print(f"Computer's first card is: {computer_initial_draw[0]}")
+    print('Player cards ', player_cards)
+    print('Player score ', player_score)
+    print('Computer\'s card ', computer_cards[0])
+    if player_score == 0 or computer_score == 0 or player_score > 21:
+        game_on = False
+    else:
+        player_should_deal = input('Type "yes" to get another card, type "n" to pass: ')
+        if player_should_deal.strip().lower() == 'y':
+            player_cards.append(draw_card(cards))
+        else:
+            game_on = False
 
-    # draw_check = input("If you want to draw, type 'y'\nIf you want to pass, type 'n'\n")
-    # if draw_check == 'y':
-    #     new_player_card = draw_card(cards)
-    #     player_initial_draw.append(new_player_card)
-    #     total_player_sum = sum(player_initial_draw)
-    #     print(f"Your next card is: {new_player_card}")
-    #     print(f" Your cards are: {player_initial_draw}")
-    #     game_on = sum_check(player_initial_draw, computer_initial_draw)
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(draw_card(cards))
+    computer_score = calculate_score(computer_cards)
+
+print(compare_score(player_score=player_score, computer_score=computer_score))
