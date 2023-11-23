1074f23b33065e34b63bd08756309ee3ca53da4a
alex <a.n.chasovskoy@gmail.com>
2023-11-23 12:59:44 +0100

    Day 17 files - some style added

--- Day 17/main.py
+++ Day 17/main.py
@@ -4,11 +4,18 @@ from models import Question, Quiz
 
 questions_data = question_data
 score = 0
-questions = [Question(question=question['text'], correct_answer=question['answer']) for question in questions_data]
+questions_bank = [Question(question=question['text'], correct_answer=question['answer']) for question in questions_data]
 
-for x in range(len(questions)):
-    quiz = Quiz(questions_list=questions)
+for x in range(len(questions_bank)):
+    quiz = Quiz(questions_list=questions_bank)
     quiz.chose_question()
+    print(f'Question {x+1}:', end=' ')
     quiz.ask_user()
     score += quiz.assessment()
-print(f'You score {score} correct answers out of {len(question_data)}')
+
+print('*' * 50)
+if score == 1:
+    print(f'You score {score} correct answer', end=' ')
+else:
+    print(f'You score {score} correct answers', end=' ')
+print(f'out of {len(question_data)}')
