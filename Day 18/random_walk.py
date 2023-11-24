e6c193b865a5c1e0532c682a4892c9265a5310c2
alex <a.n.chasovskoy@gmail.com>
2023-11-24 16:02:31 +0100

    Day 18 files - turtle drawing random walk, random rgb colors generator added

--- Day 18/random_walk.py
+++ Day 18/random_walk.py
@@ -1,17 +1,22 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 from random import choice
 
+turtle.colormode(255)
 tim = Turtle()
-tim.pensize(5)
-tim.speed(100)
-colors = ['yellow', 'gold', 'orange', 'red', 'maroon', 'violet', 'magenta', 'purple', 'navy', 'blue', 'skyblue', 'cyan',
-          'turquoise', 'lightgreen', 'green', 'darkgreen', 'chocolate', 'brown', 'black', 'gray']
 
+tim.pensize(10)
+tim.speed(10)
 directions = [0, 90, 180, 270]
 
+
+def random_color():
+    return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
+
 for _ in range(1000):
-    tim.color(choice(colors))
+    tim.pencolor(random_color())
     tim.forward(random.randint(15, 31))
     tim.setheading(random.choice(directions))
 
