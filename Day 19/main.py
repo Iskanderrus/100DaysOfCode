ba60f86d46673bd8e5191379f7a3947b34b2c07c
alex <a.n.chasovskoy@gmail.com>
2023-11-26 16:46:17 +0100

    Day 19 files - Etch-a-Sketch app final version with instructions

--- Day 19/main.py
+++ Day 19/main.py
@@ -1,8 +1,27 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
+instruction = Turtle()
+instruction.hideturtle()
+instruction.penup()
 
 screen = Screen()
+messages = ["Use 'w' to go forwards",
+            "Use 's' to go backwards",
+            "Use 'a' to turn left",
+            "Use 'd' to turn right",
+            "Use 'c' to clear the screen",
+            "Use 'h' to turn to the home position"
+            ]
+
+instruction.sety(screen.window_height() / 2 - 50)
+instruction.setx(screen.window_width() / 2 - 450)
+
+
+for message in messages:
+    instruction.write(message, move=False, align="left", font=("Arial", 12, "bold"))
+    instruction.sety(instruction.ycor() - 15)
+
 screen.listen()
 
 
@@ -38,6 +57,5 @@ screen.onkeypress(fun=turn_left, key='a')
 screen.onkeypress(fun=turn_right, key='d')
 screen.onkeypress(fun=clear_screen, key='c')
 screen.onkeypress(fun=back_home, key='h')
-
 
 screen.exitonclick()
