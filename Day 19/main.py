3c919b5883daed90549e020b5a48353f223c56c5
alex <a.n.chasovskoy@gmail.com>
2023-11-26 16:22:46 +0100

    Day 19 files - Etch-a-Sketch app

---
+++ Day 19/main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+screen = Screen()
+screen.listen()
+
+
+def turn_left():
+    tim.setheading(tim.heading() + 5)
+
+
+def turn_right():
+    tim.setheading(tim.heading() - 5)
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.forward(-10)
+
+
+def clear_screen():
+    tim.clear()
+
+
+screen.onkeypress(fun=move_forwards, key='w')
+screen.onkeypress(fun=move_backwards, key='s')
+screen.onkeypress(fun=turn_left, key='a')
+screen.onkeypress(fun=turn_right, key='d')
+screen.onkeypress(fun=clear_screen, key='c')
+
+screen.exitonclick()
