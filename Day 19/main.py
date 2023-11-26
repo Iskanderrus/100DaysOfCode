1454592f464186eecdbc74b74652ff8882386056
alex <a.n.chasovskoy@gmail.com>
2023-11-26 16:27:15 +0100

    Day 19 files - Etch-a-Sketch app final version

--- Day 19/main.py
+++ Day 19/main.py
@@ -19,11 +19,17 @@ def move_forwards():
 
 
 def move_backwards():
-    tim.forward(-10)
+    tim.backward(10)
 
 
 def clear_screen():
     tim.clear()
+
+
+def back_home():
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
 screen.onkeypress(fun=move_forwards, key='w')
@@ -31,5 +37,7 @@ screen.onkeypress(fun=move_backwards, key='s')
 screen.onkeypress(fun=turn_left, key='a')
 screen.onkeypress(fun=turn_right, key='d')
 screen.onkeypress(fun=clear_screen, key='c')
+screen.onkeypress(fun=back_home, key='h')
+
 
 screen.exitonclick()
