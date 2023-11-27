62ab67e5f11645652e1edbc0b3070b49295c23de
alex <a.n.chasovskoy@gmail.com>
2023-11-27 15:36:53 +0100

    Day 20 files - Snake Game - cleanup in models

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -2,10 +2,29 @@
 from turtle import Turtle
 
 
-class Snake(Turtle):
+class Snake:
     def __init__(self):
         super().__init__()
-        self.shape('square')
-        self.penup()
-        self.color('white')
-        self.shapesize(0.5, 0.5)
+        self.segment = None
+        self.snake_body = []
+        self.home_x = 0
+        for x in range(3):
+            self.eat(x)
+
+    def move(self):
+        for seg_num in range(len(self.snake_body) - 1, 0, -1):
+            new_pos = self.snake_body[seg_num - 1].pos()
+            self.snake_body[seg_num].goto(new_pos)
+
+        self.snake_body[0].forward(10)
+
+    def eat(self, x):
+        self.segment = Turtle()
+        self.segment.setx(self.home_x - 10 * x)
+
+        self.segment.shape('square')
+        self.segment.penup()
+        self.segment.color('white')
+        self.segment.shapesize(0.5, 0.5)
+
+        self.snake_body.append(self.segment)
--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -1,11 +1,10 @@
 # בס״ד
 import time
-from random import randint
-from turtle import Turtle, Screen
-from score_board import ScoreBoard
-from food import Food
-from snake import Snake
+from turtle import Screen
 
+from food import Food
+from score_board import ScoreBoard
+from snake import Snake
 
 score = 0
 
@@ -18,33 +17,21 @@ screen.tracer(0)
 
 # Scoreboard initialization
 scoreboard = ScoreBoard()
-scoreboard.goto(scoreboard.shapesize()[1] / 2, screen.window_height()/2 - 35)
+scoreboard.goto(scoreboard.shapesize()[1] / 2, screen.window_height() / 2 - 35)
 
 # Food initialize
 food = Food()
 
 # Set up the snake
-snake_body = []
-home_x = 0
-
-for x in range(3):
-    segment = Snake()
-    segment.setx(home_x - 10 * x)
-    snake_body.append(segment)
+snake = Snake()
 
 # Making the snake move
 game_is_on = True
-
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     scoreboard.write(f"Score: {score}", align='center', font=('Arial', 20, 'bold'))
-    for seg_num in range(len(snake_body) - 1, 0, -1):
-        new_pos = snake_body[seg_num - 1].pos()
-        snake_body[seg_num].goto(new_pos)
-
-    snake_body[0].forward(10)
-
+    snake.move()
 
 screen.exitonclick()
