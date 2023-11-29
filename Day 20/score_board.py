4f22a13cee64a0533f875ef1677ba6aa0591307c
alex <a.n.chasovskoy@gmail.com>
2023-11-29 16:46:12 +0100

    Day 24 files - final version

--- Day 20/score_board.py
+++ Day 20/score_board.py
@@ -2,10 +2,10 @@
 from pathlib import Path
 from turtle import Turtle
 
-path = Path('data/high_score.txt')
-
 
 class ScoreBoard(Turtle):
+    HIGH_SCORE_FILE_PATH = Path('data/high_score.txt')
+
     def __init__(self, user_name):
         super().__init__()
         self.hideturtle()
@@ -14,13 +14,14 @@ class ScoreBoard(Turtle):
         self.color('white')
         self.score = 0
         self.user_name = user_name.strip().title()
-        if path.exists():
-            contents = path.read_text()
+
+        if ScoreBoard.HIGH_SCORE_FILE_PATH.exists():
+            contents = ScoreBoard.HIGH_SCORE_FILE_PATH.read_text()
             self.high_score = int(contents)
         else:
             self.high_score = 0
-            Path('data').mkdir(parents=True)
-            Path('data/high_score.txt').touch()
+            ScoreBoard.HIGH_SCORE_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+            ScoreBoard.HIGH_SCORE_FILE_PATH.touch()
         self.message()
 
     def message(self):
@@ -38,6 +39,6 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            path.write_text(str(self.high_score))
+            ScoreBoard.HIGH_SCORE_FILE_PATH.write_text(str(self.high_score))
         self.score = 0
         self.message()
--- Day 24/main.py
+++ Day 24/main.py
@@ -1,4 +1,23 @@
-with open('my_data.txt', 'r+') as f:
-    contents = f.readlines()
-    for row in contents:
-        print(row)
+# בס״ד
+
+from pathlib import Path
+
+# Reading names from the file
+names_path = Path('./customers/names.txt')
+names_contents = names_path.read_text()
+names = names_contents.splitlines()
+
+# Reading message blueprint
+blueprint_path = Path('./input/blueprint.txt')
+blueprint = blueprint_path.read_text()
+
+# Generating personalized message and saving it to the individual file
+for name in names:
+    # replacing the placeholder
+    message = blueprint.replace('[NAME]', str(name))
+    # creating individual path
+    path = Path(f'output/letter_for_{name}.txt')
+    # create empty file
+    path.touch()
+    # writing the personalized message into individual file
+    path.write_text(message)
