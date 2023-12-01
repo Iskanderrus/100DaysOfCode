d8376702430ce2f121c40d9b24887dc8ccaaf1db
alex <a.n.chasovskoy@gmail.com>
2023-12-01 15:46:45 +0100

    Day 25 files - squirrels colors saved to csv

--- Day 25/main.py
+++ Day 25/main.py
@@ -2,9 +2,11 @@
 
 import pandas as pd
 
-raw_data = pd.read_csv("../../../Desktop/100 Days of Code The Complete Python Pro Bootcamp for 2023/25 - Day 25 - Intermediate - Working with CSV Data and the Pandas Library/002 weather-data.csv")
+raw_data = pd.read_csv("../../../Desktop/100 Days of Code The Complete Python Pro Bootcamp for 2023/25 - Day 25 - "
+                       "Intermediate - Working with CSV Data and the Pandas Library/002 weather-data.csv")
 
 df = pd.DataFrame(raw_data)
+avg_temp = round(df.temp.mean())
 
-temperatures = df.temp
-print(temperatures)
+print(df[df.temp == df.temp.max()]['temp'])
+print(avg_temp)
---
+++ Day 25/squirrels.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = pd.read_csv("../../../Desktop/100 Days of Code The Complete Python Pro Bootcamp for 2023/25 - Day 25 - Intermediate - Working with CSV Data and the Pandas Library/004 2018-Central-Park-Squirrel-Census-Squirrel-Data.csv")
+df = pd.DataFrame(data)
+
+print(df.head())
+colors_df = df["Highlight Fur Color"].value_counts()
+print(colors_df)
+
+colors_df.to_csv('colors_data.csv')
