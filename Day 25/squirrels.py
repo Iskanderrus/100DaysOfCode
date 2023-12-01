1fd4a04fb63568f13bc61830c406cb07c7ee35d8
alex <a.n.chasovskoy@gmail.com>
2023-12-01 15:53:15 +0100

    Day 25 files - even shorter version

--- Day 25/squirrels.py
+++ Day 25/squirrels.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 data = pd.read_csv("../../../Desktop/100 Days of Code The Complete Python Pro Bootcamp for 2023/25 - Day 25 - Intermediate - Working with CSV Data and the Pandas Library/004 2018-Central-Park-Squirrel-Census-Squirrel-Data.csv")
+
 df = pd.DataFrame(data)
 
-print(df.head())
-colors_df = df["Primary Fur Color"].value_counts()
-print(colors_df)
+#print(df.head())
+#colors_df = df["Primary Fur Color"].value_counts()
+#print(colors_df)
 
-colors_df.to_csv('colors_data.csv')
+#colors_df.to_csv('colors_data.csv')
+
+df["Primary Fur Color"].value_counts().to_csv('colors_data.csv')
