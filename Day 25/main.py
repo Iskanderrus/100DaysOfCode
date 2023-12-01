e9292c0a356eda507d4a346a4b439433d1bf0f74
alex <a.n.chasovskoy@gmail.com>
2023-12-01 14:36:00 +0100

    Day 25 files - reading csv data with pandas

---
+++ Day 25/main.py
@@ -0,0 +1,10 @@
+# בס״ד
+
+import pandas as pd
+
+raw_data = pd.read_csv("../../../Desktop/100 Days of Code The Complete Python Pro Bootcamp for 2023/25 - Day 25 - Intermediate - Working with CSV Data and the Pandas Library/002 weather-data.csv")
+
+df = pd.DataFrame(raw_data)
+
+temperatures = df.temp
+print(temperatures)
