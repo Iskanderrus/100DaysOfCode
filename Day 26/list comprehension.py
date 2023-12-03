70a0f0bcaa0ef3d51737ae5a0496cdc0035338c9
alex <a.n.chasovskoy@gmail.com>
2023-12-03 22:28:44 +0100

    Day 26 files - List comprehension - squared numbers challenge

--- Day 26/list comprehension.py
+++ Day 26/list comprehension.py
@@ -3,8 +3,13 @@
 # Blueprint: new_list = [new_item] for item in list
 
 numbers = range(1, 11)
-squared_numbers = [x**2 for x in numbers]
+squared_numbers = [x ** 2 for x in numbers]
 
-sqared_sqared_even_numbers = [y**2 for y in [x**2 for x in numbers] if y%2==0]
+# if condition in list comprehension
+sqared_sqared_even_numbers = [y ** 2 for y in [x ** 2 for x in numbers] if y % 2 == 0]
+
 print(squared_numbers)
 print(sqared_sqared_even_numbers)
+
+names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+print([name.upper() for name in names if len(name) > 5])
