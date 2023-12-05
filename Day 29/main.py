51e9b7a7cd40e9dbdb8fcdb40fd0ae77dd56c9b5
alex <a.n.chasovskoy@gmail.com>
2023-12-05 22:28:30 +0100

    Day 29 files - Password Manager - final version

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,13 +1,112 @@
 # בס״ד
+import os
+import random
+import string
 import sys
-from tkinter import Tk, Label, Button, PhotoImage, Canvas, Entry
+from pathlib import Path
+from tkinter import Tk, Label, Button, PhotoImage, Canvas, Entry, simpledialog, messagebox
+
+import pyperclip
 from PIL import Image, ImageTk
 
 FONT_NAME = 'TkMenuFont'
 
+# default values for the password
+lower_case_number = 6
+upper_case_number = 4
+digits_number = 5
+symbols_number = 4
+
 
 def exit_button():
+    """
+    Function to exit from the app saving the password if generated to ensure that the password is not get lost
+    :return:
+    """
+    if password_entry.get():
+        adding_password()
+
     sys.exit()
+
+
+def special_requirements():
+    """
+    Function to adjust the default password settings according to user requirements.
+    User is prompted for each symbol group
+    :return:
+    """
+    global lower_case_number, upper_case_number, digits_number, symbols_number
+    lower_case_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many lowercase characters are required?"))
+
+    upper_case_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many uppercase characters are required?"))
+
+    digits_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many digits are required?"))
+
+    symbols_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many symbols are required?"))
+
+
+def check_credentials():
+    """ Function to check if user entered website or username/email"""
+    if len(website_entry.get()) <= 4 or not email_entry.get():
+        # if one of the values is missing message popup
+        messagebox.showinfo("Missing data", "Please check credentials")
+    else:
+        return True
+
+
+def create_password():
+    """
+    Main function to generate password.
+    :return:
+    """
+    global lower_case_number, upper_case_number, digits_number, symbols_number
+    # empty the field in case user wants to regenerate the password
+    password_entry.delete(0, 'end')
+
+    if check_credentials():
+        try:
+            new_password = (
+                    random.sample(string.ascii_lowercase, k=lower_case_number) +
+                    random.sample(string.ascii_uppercase, k=upper_case_number) +
+                    random.sample(string.digits, k=digits_number) +
+                    random.sample(string.punctuation, k=symbols_number))
+        except (TypeError, ValueError):
+            sys.exit()
+        else:
+            random.shuffle(new_password)
+            random.shuffle(new_password)
+            password_str = ''.join(new_password)
+            # insert the generated password into the entry field
+            password_entry.insert(0, password_str)
+
+
+def adding_password():
+    """
+    Function aiming two goals:
+    1. Add the new website, user data and password to the file for storing
+    2. Copying the newly generated password to clipboard for immediate use after saving
+    :return:
+    """
+    if check_credentials():
+        path_folder = Path('./output')
+        path = Path(f'{path_folder}/my_passwords.txt')
+        message = str(f'{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n')
+        if not path.exists():
+            Path.mkdir(path_folder, parents=True)
+            path.touch()
+
+        with open(path, mode='a') as file:
+            file.write(message)
+        pyperclip.copy(password_entry.get())
+        password_entry.delete(0, 'end')
 
 
 # create main window
@@ -16,7 +115,9 @@ window.title('Password manager')
 window.config(padx=20, pady=20, bg='#BEFBFF')
 
 # add icon
-ico = Image.open('images/logo_image.png')
+image_path = './images/logo_image.png'
+
+ico = Image.open(image_path)
 photo = ImageTk.PhotoImage(ico)
 window.wm_iconphoto(False, photo)
 
@@ -28,6 +129,7 @@ canvas = Canvas(
     height=400,
     highlightthickness=0
 )
+
 filename = PhotoImage(file="images/bg_image.png")
 canvas.create_image(225, 230, image=filename)
 canvas.create_text(
@@ -65,9 +167,23 @@ generate_button = Button(
     activeforeground='#F9E3E2',
     highlightthickness=0,
     text='Generate Password',
+    command=create_password,
 )
-
 generate_button.grid(row=4, column=2)
+
+special_requirements_button = Button(
+    window,
+    font=FONT_NAME,
+    height=1,
+    bg='#C06600',
+    fg='#F9E3E2',
+    activebackground='#169700',
+    activeforeground='#F9E3E2',
+    highlightthickness=0,
+    text='Requirements',
+    command=special_requirements,
+)
+special_requirements_button.grid(row=0, column=0)
 
 add_button = Button(
     window,
@@ -78,7 +194,8 @@ add_button = Button(
     activebackground='#169700',
     activeforeground='#F9E3E2',
     highlightthickness=0,
-    text='Add Password'
+    text='Add Password',
+    command=adding_password,
 )
 
 add_button.grid(row=5, column=1, columnspan=2)
@@ -88,7 +205,7 @@ website_label = Label(
     window,
     padx=1,
     text='Website:',
-    font=(FONT_NAME, 12),
+    font=(FONT_NAME, 10),
     bg='#BEFBFF',
     fg='#281713',
 )
@@ -99,7 +216,7 @@ email_label = Label(
     window,
     padx=1,
     text='Email/Username:',
-    font=(FONT_NAME, 12),
+    font=(FONT_NAME, 10),
     bg='#BEFBFF',
     fg='#281713',
 )
@@ -109,7 +226,7 @@ password_label = Label(
     window,
     padx=1,
     text='Password:',
-    font=(FONT_NAME, 12),
+    font=(FONT_NAME, 10),
     bg='#BEFBFF',
     fg='#281713',
 )
@@ -117,7 +234,8 @@ password_label.grid(row=4, column=0)
 
 # text fields
 website_entry = Entry(window, width=52)
-website_entry.insert(0, 'https://')
+website_entry.focus()
+website_entry.insert(0, 'www.')
 website_entry.grid(row=2, column=1, columnspan=2, ipady=3, pady=5)
 
 email_entry = Entry(window, width=52)
