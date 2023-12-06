1beb74754ac6fbd95e37472050fdee4507526a98
alex <a.n.chasovskoy@gmail.com>
2023-12-06 11:57:21 +0100

    Day 26 files - error handling added

--- Day 26/spelling.py
+++ Day 26/spelling.py
@@ -10,19 +10,29 @@ archive = zipfile.ZipFile('../../../Desktop/100 Days of Code The Complete Python
 raw_data = archive.read('NATO-alphabet-start/nato_phonetic_alphabet.csv')
 
 # converting bites into the string
-string_data = str(raw_data,'utf-8')
+string_data = str(raw_data, 'utf-8')
 data = StringIO(string_data)
 
 # creating dataframe from the string object
 df = pd.read_csv(data)
-#print(df.head())
-
-# asking user for a word to spell
-user_input = input('What word do you want to spell?\n').strip().upper()
+# print(df.head())
 
 # creating a dictionary from the dataframe
 spelling_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
-# creating a list from the user input and the dictionary
-spelling = [f'{letter}: {spelling_dict[letter]}' for letter in user_input]
-print(spelling)
+
+def generate_phonetics(dict_of_letters):
+    # asking user for a word to spell
+    user_input = input('What word do you want to spell?\n').strip().upper()
+    # creating a list from the user input and the dictionary
+    try:
+        spelling = [f'{letter}: {dict_of_letters[letter]}' for letter in user_input]
+    # if user input contains numbers or symbols:
+    except KeyError:
+        print('Sorry, only letters in the alphabet, please.\n')
+        generate_phonetics(dict_of_letters)
+    else:
+        print(spelling)
+
+
+generate_phonetics(spelling_dict)
