eb4389673294cb5ac8f14959e6e1c4cbacce31a6
alex <a.n.chasovskoy@gmail.com>
2023-12-06 12:51:06 +0100

    Day 30 files - error handling added

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,33 +1,269 @@
 # בס״ד
+import datetime
+import json
+import random
+import string
+import sys
+from pathlib import Path
+from tkinter import Tk, Label, Button, PhotoImage, Canvas, Entry, simpledialog, messagebox
 
-fruits = ['Apple', 'Pear', 'Orange']
+import pyperclip
+from PIL import Image, ImageTk
+
+FONT_NAME = 'TkMenuFont'
+DATA_FILE = Path('data.json')
+    #Path('../../../Documents/password_manager_log.json'))
+
+# default values for the password
+lower_case_number = 6
+upper_case_number = 4
+digits_number = 5
+symbols_number = 4
 
 
-def make_pie(index):
-    try:
-        fruit = fruits[index]
-    except IndexError as error_message:
-        print(f'Code has an error: {error_message}. Looks like the index {index} is wrong.')
+def exit_button():
+    """
+    Function to exit from the app saving the password if generated to ensure that the password is not get lost
+    :return:
+    """
+    if password_entry.get():
+        adding_password()
+
+    sys.exit()
+
+
+def special_requirements():
+    """
+    Function to adjust the default password settings according to user requirements.
+    User is prompted for each symbol group
+    :return:
+    """
+    global lower_case_number, upper_case_number, digits_number, symbols_number
+    lower_case_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many lowercase characters are required?"))
+
+    upper_case_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many uppercase characters are required?"))
+
+    digits_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many digits are required?"))
+
+    symbols_number = int(simpledialog.askstring(
+        title="Number required!",
+        prompt="How many symbols are required?"))
+
+
+def check_credentials():
+    """ Function to check if user entered website or username/email"""
+    if len(website_entry.get()) <= 4 or not email_entry.get():
+        # if one of the values is missing message popup
+        messagebox.showinfo("Missing data", "Please check credentials")
     else:
-        print(fruit + 'pie')
+        return True
 
 
-make_pie(4)
+def create_password():
+    """
+    Main function to generate password.
+    :return:
+    """
+    global lower_case_number, upper_case_number, digits_number, symbols_number
+    # empty the field in case user wants to regenerate the password
+    password_entry.delete(0, 'end')
 
-facebook_posts = [
-    {'Likes': 21, 'Comments': 2},
-    {'Likes': 13, 'Comments': 2, 'Shares': 1},
-    {'Likes': 33, 'Comments': 8, 'Shares': 3},
-    {'Comments': 4, 'Shares': 2},
-    {'Comments': 1, 'Shares': 1},
-    {'Likes': 19, 'Comments': 3},
-]
+    if check_credentials():
+        try:
+            new_password = (
+                    random.sample(string.ascii_lowercase, k=lower_case_number) +
+                    random.sample(string.ascii_uppercase, k=upper_case_number) +
+                    random.sample(string.digits, k=digits_number) +
+                    random.sample(string.punctuation, k=symbols_number))
+        except (TypeError, ValueError):
+            sys.exit()
+        else:
+            random.shuffle(new_password)
+            random.shuffle(new_password)
+            password_str = ''.join(new_password)
+            # insert the generated password into the entry field
+            password_entry.insert(0, password_str)
 
-total_likes = 0
 
-for post in facebook_posts:
-    try:
-        total_likes += post['Likes']
-    except KeyError:
-        total_likes += 0
-print(f'Total number of likes: {total_likes}')
+def adding_password():
+    """
+    Function aiming two goals:
+    1. Add the new website, user data and password to the file for storing
+    2. Copying the newly generated password to clipboard for immediate use after saving
+    :return:
+    """
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    date = datetime.datetime.now()
+    current_date = date.strftime('%d.%m.%Y')
+
+    new_entry = {
+        website: {
+            "username": email,
+            "password": password,
+            "logged_date": current_date
+        }
+    }
+    if check_credentials():
+        try:
+            with open(DATA_FILE, 'r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open(DATA_FILE, 'w') as data_file:
+                json.dump(new_entry, data_file, indent=4)
+        else:
+            data.update(new_entry)
+
+            with open(DATA_FILE, 'w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            pyperclip.copy(password_entry.get())
+            messagebox.showinfo("Success", "Saved and copied to clipboard")
+            password_entry.delete(0, 'end')
+            website_entry.delete(4, 'end')
+
+
+# create main window
+window = Tk()
+window.title('Password manager')
+window.config(padx=20, pady=20, bg='#BEFBFF')
+
+# add icon
+image_path = './images/logo_image.png'
+
+ico = Image.open(image_path)
+photo = ImageTk.PhotoImage(ico)
+window.wm_iconphoto(False, photo)
+
+# create and setup canvas
+canvas = Canvas(
+    window,
+    bg='#BEFBFF',
+    width=400,
+    height=400,
+    highlightthickness=0
+)
+
+filename = PhotoImage(file="images/bg_image.png")
+canvas.create_image(225, 230, image=filename)
+canvas.create_text(
+    220, 30,
+    text='Create, save, manage and securely use safe passwords',
+    width=360,
+    font=(FONT_NAME, 13, 'bold'),
+    fill='#281713',
+    justify='center'
+)
+canvas.grid(row=1, column=1)
+
+# add buttons
+exit_img = Image.open('images/exit.png')
+exit_photo = ImageTk.PhotoImage(exit_img)
+exit_button = Button(
+    window,
+    image=exit_photo,
+    bg='#BEFBFF',
+    height=30,
+    width=65,
+    highlightthickness=0,
+    command=exit_button,
+    anchor='se'
+)
+exit_button.grid(row=0, column=2, sticky='se')
+
+generate_button = Button(
+    window,
+    font=FONT_NAME,
+    height=1,
+    bg='#0089B4',
+    fg='#F9E3E2',
+    activebackground='#169700',
+    activeforeground='#F9E3E2',
+    highlightthickness=0,
+    text='Generate Password',
+    command=create_password,
+)
+generate_button.grid(row=4, column=2)
+
+special_requirements_button = Button(
+    window,
+    font=FONT_NAME,
+    height=1,
+    bg='#C06600',
+    fg='#F9E3E2',
+    activebackground='#169700',
+    activeforeground='#F9E3E2',
+    highlightthickness=0,
+    text='Requirements',
+    command=special_requirements,
+)
+special_requirements_button.grid(row=0, column=0)
+
+add_button = Button(
+    window,
+    font=FONT_NAME,
+    width=51,
+    bg='#0089B4',
+    fg='#F9E3E2',
+    activebackground='#169700',
+    activeforeground='#F9E3E2',
+    highlightthickness=0,
+    text='Add Password',
+    command=adding_password,
+)
+
+add_button.grid(row=5, column=1, columnspan=2)
+
+# Labels
+website_label = Label(
+    window,
+    padx=1,
+    text='Website:',
+    font=(FONT_NAME, 10),
+    bg='#BEFBFF',
+    fg='#281713',
+)
+
+website_label.grid(row=2, column=0)
+
+email_label = Label(
+    window,
+    padx=1,
+    text='Email/Username:',
+    font=(FONT_NAME, 10),
+    bg='#BEFBFF',
+    fg='#281713',
+)
+email_label.grid(row=3, column=0)
+
+password_label = Label(
+    window,
+    padx=1,
+    text='Password:',
+    font=(FONT_NAME, 10),
+    bg='#BEFBFF',
+    fg='#281713',
+)
+password_label.grid(row=4, column=0)
+
+# text fields
+website_entry = Entry(window, width=52)
+website_entry.focus()
+website_entry.insert(0, 'www.')
+website_entry.grid(row=2, column=1, columnspan=2, ipady=3, pady=5)
+
+email_entry = Entry(window, width=52)
+email_entry.insert(0, '@')
+email_entry.grid(row=3, column=1, columnspan=2, ipady=3, pady=5)
+
+password_entry = Entry(window, width=35)
+password_entry.grid(row=4, column=1, ipady=3, pady=5)
+
+window.mainloop()
