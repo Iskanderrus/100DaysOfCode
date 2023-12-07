b00bd15eac68a0d8ffe060c655c7061d6cfb3f5e
alex <a.n.chasovskoy@gmail.com>
2023-12-07 12:37:06 +0100

    Day 31 files - full GUI, no logic

--- Day 31/serbian_flash_cards.py
+++ Day 31/serbian_flash_cards.py
@@ -1,19 +1,31 @@
 # בס״ד
 from tkinter import Tk, Widget, Button, Entry, Label, Canvas, PhotoImage
 
+from PIL import Image, ImageTk
+
+
+BACKGROUND_COLOR = "#B1DDC6"
+
 # main window
 root = Tk()
 root.title('Карточки сербского языка')
+root.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # canvas
 canvas = Canvas(root, height=600, width=600, highlightthickness=0)
 filename = PhotoImage('images/card_front.png')
 canvas.create_image(300, 300, image=filename)
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=3)
 
 # buttons
+right_photo = PhotoImage(file='images/right.png')
+right_button = Button(root, image=right_photo, bg=BACKGROUND_COLOR, borderwidth=0)
+right_button.config( borderwidth=0)
+right_button.grid(row=2, column=2)
 
 
-
+wrong_photo = PhotoImage(file='images/wrong.png')
+wrong_button = Button(root, image=wrong_photo, bg=BACKGROUND_COLOR, borderwidth=0)
+wrong_button.grid(padx=20, pady=20, row=2, column=4)
 
 root.mainloop()
