0d8468759c8da252cf49b57f06f57c6e90a85654
alex <a.n.chasovskoy@gmail.com>
2023-12-13 20:42:55 +0100

    Day 32 - initial commit

--- Day 31/sound_generation.py
+++ Day 31/sound_generation.py
@@ -8,7 +8,7 @@ df = pd.read_csv('data/alphabet.csv')
 words = list(df.sample_latinica)
 
 for word in words:
-#    tts = gtts.gTTS(f"{word}", lang="sr")
-#    tts.save(f"sounds/{word}.mp3")
-    playsound(f"sounds/{word}.mp3")
-    time.sleep(1)
+    tts = gtts.gTTS(f"{word}", lang="sr")
+    tts.save(f"sounds/{word}.mp3")
+#    playsound(f"sounds/{word}.mp3")
+#    time.sleep(1)
