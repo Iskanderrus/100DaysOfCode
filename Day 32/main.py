ae559d488648f393d39b9ffa7bbafdd97437462a
alex <a.n.chasovskoy@gmail.com>
2023-12-13 21:15:19 +0100

    Day 32 - with statement added

--- Day 32/main.py
+++ Day 32/main.py
@@ -4,11 +4,11 @@ my_email = 'alex_ru2002@list.ru'
 with open('../../../Documents/password.txt') as f:
     my_password = f.read()
 
-connection = smtplib.SMTP_SSL('smtp.mail.ru')
-connection.login(user=my_email, password=my_password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs='a.n.chasovskoy@gmail.com',
-    msg='Subject:Hi there!\n\nThis is the body of my email'
-)
-connection.close()
+with smtplib.SMTP_SSL('smtp.mail.ru') as connection:
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs='a.n.chasovskoy@gmail.com',
+        msg='Subject:Hi there!\n\nThis is the body of my email'
+    )
+
