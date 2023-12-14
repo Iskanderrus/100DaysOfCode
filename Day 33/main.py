9478aaf36b340443a16abdfbc4b1a6863e681b51
alex <a.n.chasovskoy@gmail.com>
2023-12-14 12:49:30 +0100

    Day 33 - basic requests intro

--- Day 33/main.py
+++ Day 33/main.py
@@ -2,6 +2,13 @@ import requests
 
 response = requests.get('http://api.open-notify.org/iss-now.json')
 # print the response code
-print(response)
+print(response.status_code)
+# raise an error message in case connection is not successful
+response.raise_for_status()
 # print the response json
-print(response.json())
+data = response.json()
+print(data)
+
+# iss position
+iss_position = data['iss_position']
+print(iss_position)
