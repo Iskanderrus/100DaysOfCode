022a84c2327ee44de4e88132e5fb0cf367fda609
alex <a.n.chasovskoy@gmail.com>
2023-12-17 23:06:07 +0100

    Day 35 - simple weather request

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,1 +1,20 @@
 # בס״ד
+import requests
+
+with open('key.txt', 'r') as f:
+    API_KEY = f.read().strip()
+
+url = 'https://api.openweathermap.org/data/2.5/forecast'
+parameters = {
+    'lat': 46.104495,
+    'lon': 19.663891,
+    'appid': API_KEY,
+    'units': 'metric',
+    'lang': 'ru'
+}
+
+response = requests.get(url=url, params=parameters)
+response.raise_for_status()
+
+data = response.json()
+print(data)
