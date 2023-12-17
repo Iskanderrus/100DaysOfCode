dd798fb37f7e78d129257e63e7403c8a7f747cab
alex <a.n.chasovskoy@gmail.com>
2023-12-17 23:54:41 +0100

    Day 35 - some refactoring

--- Day 35/main.py
+++ Day 35/main.py
@@ -6,6 +6,7 @@ import requests
 with open('key.txt', 'r') as f:
     API_KEY = f.read().strip()
 
+# weather forecast for the next 5 days - forested for every 3 hours
 url = 'https://api.openweathermap.org/data/2.5/forecast'
 parameters = {
     'lat': 46.104495,
@@ -19,11 +20,19 @@ response = requests.get(url=url, params=parameters)
 response.raise_for_status()
 
 data = response.json()
-
-for hour in data['list']:
+# use only next 12 hours
+for hour in data['list'][0:5]:
     # grab all hours and split by days
+    condition_code = hour['weather'][0]['id']
+    hour_str = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(hour['dt'])).split(' ')[1].split(':')[0]
+    forecast_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(hour['dt']))
+    description = hour['weather'][0]['description']
+
     if data['list'].index(hour) % 8 == 0:
         print()
     # if weather id less than 700 and time is between 6 am and 4 pm - give a message
-    if hour['weather'][0]['id'] < 700 and 16 > int(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(hour['dt'])).split(' ')[1].split(':')[0]) > 6:
-        print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(hour['dt'])), ' возьми зонт: ', hour['weather'][0]['description'])
+    if (condition_code < 700 and
+            16 > int(hour_str) > 6):
+        print(forecast_time,
+              ' возьми зонт: ',
+              description)
