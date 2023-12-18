d5acfff7c422b078da7625d33f16b4cdcf6ec0b3
alex <a.n.chasovskoy@gmail.com>
2023-12-19 00:45:23 +0100

    Day 36 - using API to retrieve data and pandas to get difference

--- Day 36/main.py
+++ Day 36/main.py
@@ -1,8 +1,42 @@
 # בס״ד
+import os
+import pandas as pd
+import requests
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 ## STEP 1: Use https://www.alphavantage.co
+stock_key = os.environ['ALPHA_VANTAGE_API_KEY']
+
+url = 'https://www.alphavantage.co/query'
+parameters = {
+    'function': 'TIME_SERIES_DAILY',
+    'symbol': STOCK,
+    'apikey': stock_key
+}
+r = requests.get(url, params=parameters)
+r.raise_for_status()
+data = r.json()
+df = pd.DataFrame(data['Time Series (Daily)'])
+df = df.T
+df.rename(columns={
+    '1. open': 'open',
+    '2. high': 'high',
+    '3. low': 'low',
+    '4. close': 'close',
+    '5. volume': 'volume'
+},
+    inplace=True
+)
+
+df = df.astype(float)
+
+df['diff'] = df['close'].diff()
+df['pct_change'] = df['close'].pct_change()
+
+print(df[abs(df['pct_change']) > 0.05])
+
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 ## STEP 2: Use https://newsapi.org
@@ -12,7 +46,7 @@ COMPANY_NAME = "Tesla Inc"
 # Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this:
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
