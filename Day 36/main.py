801c47e02ef61852b429d270df18759b97e0c95f
alex <a.n.chasovskoy@gmail.com>
2023-12-19 16:12:00 +0100

    Day 36 - final version

--- Day 36/main.py
+++ Day 36/main.py
@@ -1,66 +1,74 @@
 # בס״ד
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-import pandas as pd
 import requests
-
-# STOCK = "TSLA"
-# COMPANY_NAME = "Tesla Inc"
-#
-# ## STEP 1: Use https://www.alphavantage.co
-# stock_key = os.environ['ALPHA_VANTAGE_API_KEY']
-#
-# url = 'https://www.alphavantage.co/query'
-# parameters = {
-#     'function': 'TIME_SERIES_DAILY',
-#     'symbol': STOCK,
-#     'apikey': stock_key
-# }
-# r = requests.get(url, params=parameters)
-# r.raise_for_status()
-# data = r.json()
-# df = pd.DataFrame(data['Time Series (Daily)'])
-# df = df.T
-# df.rename(columns={
-#     '1. open': 'open',
-#     '2. high': 'high',
-#     '3. low': 'low',
-#     '4. close': 'close',
-#     '5. volume': 'volume'
-# },
-#     inplace=True
-# )
-#
-# df = df.astype(float)
-#
-# df['diff'] = df['close'].diff()
-# df['pct_change'] = df['close'].pct_change()
-# df = df[abs(df['pct_change']) >= 0.05]
-# df.to_csv('tesla_data.csv')
-df = pd.read_csv('tesla_data.csv')
-df.rename(columns={'Unnamed: 0': 'date'}, inplace=True)
-df['date'] = pd.to_datetime(df['date'], format='%Y-%M-%d').dt.to_period(freq="D")
-dates = df.date
-
-print(dates)
-
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
+from newsapi import NewsApiClient
+from twilio.rest import Client
 
 
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+# twilio credentials
+ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
+AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
+US_PHONE_NUMBER = os.environ.get('US_PHONE_NUMBER')
+
+# other API keys
+stock_key = os.environ['ALPHA_VANTAGE_API_KEY']
+news_key = os.environ.get('NEWS_API_KEY')
+
+# getting yesterday and day before yesterday dates
+yesterday = datetime.now() - timedelta(1)
+day_before_yesterday = datetime.now() - timedelta(2)
+
+# handling weekend
+yesterday_weekno = yesterday.weekday()
+day_before_yesterday_weekno = day_before_yesterday.weekday()
+
+if yesterday_weekno == 5:
+    yesterday = yesterday - timedelta(1)
+elif yesterday_weekno == 6:
+    yesterday = yesterday - timedelta(2)
+
+if day_before_yesterday_weekno == 5:
+    day_before_yesterday = day_before_yesterday - timedelta(1)
+elif day_before_yesterday_weekno == 6:
+    day_before_yesterday = day_before_yesterday - timedelta(2)
+
+yesterday = str(yesterday).split(' ')[0]
+day_before_yesterday = str(day_before_yesterday).split(' ')[0]
+
+# requesting market data
+url = 'https://www.alphavantage.co/query'
+parameters = {
+     'function': 'TIME_SERIES_DAILY',
+     'symbol': STOCK,
+     'apikey': stock_key
+}
+r = requests.get(url, params=parameters)
+r.raise_for_status()
+data = r.json()
+
+# getting closing price
+yesterday_close = float(data['Time Series (Daily)'][yesterday]['4. close'])
+day_before_yesterday_close = float(data['Time Series (Daily)'][day_before_yesterday]['4. close'])
+
+price_difference = abs(round(100-(yesterday_close/day_before_yesterday_close * 100), 2))
+
+# if price difference is over 5%, getting the news and sending three news titles and brief descriptions and links
+if price_difference >= 5:
+    newsapi = NewsApiClient(api_key=news_key)
+    all_articles = newsapi.get_everything(q=COMPANY_NAME,
+                                          from_param=day_before_yesterday)
+
+    articles = all_articles['articles'][:3]
+    for article in articles:
+        client = Client(ACCOUNT_SID, AUTH_TOKEN)
+        message = client.messages.create(
+            body=f'{article["title"]}\n{article["description"]}\n{article["url"]}',
+            from_=US_PHONE_NUMBER,
+            to='+381629448617'
+        )
+        print(message.status)
