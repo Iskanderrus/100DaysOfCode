5f0614374abf824415455dcaf4d91c1ccd932a97
alex <a.n.chasovskoy@gmail.com>
2023-12-25 19:15:16 +0100

    Day 47 - simple Amazon scraper and price change notifier

---
+++ Day 47/main.py
@@ -0,0 +1,35 @@
+# בס״ד
+import smtplib
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1'
+# HEADERS = ({'User-Agent':
+#             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+#             'Accept-Language': 'ru-RU,ru;q=0.8'})
+GOAL_PRICE = 100
+
+response = requests.get(url=URL)#, headers=HEADERS)
+response.raise_for_status()
+data = response.text
+
+soup = BeautifulSoup(data, features='html.parser')
+try:
+    price = float(soup.select_one('div.a-row span.a-price span.a-offscreen').getText().strip('$'))
+except Exception:
+    print('Check response or page was changed.')
+else:
+    if price < GOAL_PRICE:
+        with smtplib.SMTP_SSL('smtp.mail.ru') as connection:
+            my_email = 'alex_ru2002@list.ru'
+            with open('../../../Documents/password.txt') as f:
+                my_password = f.read()
+
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs='a.n.chasovskoy@gmail.com',
+                msg=f'Subject: Shopping Time\n\n\nCheck the page {URL} '
+                    f'Current price: ${price}'
+            )
