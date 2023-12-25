8c5819520a7fecb087c8fc6835b5b1f674408c38
alex <a.n.chasovskoy@gmail.com>
2023-12-25 11:16:22 +0100

    Day 46 - scraping top 100 songs on specific date - refactored

--- Day 46/main.py
+++ Day 46/main.py
@@ -10,10 +10,6 @@ response.raise_for_status()
 data = response.text
 
 soup = BeautifulSoup(data, features='html.parser')
-hits = soup.find_all(name='ul', class_='lrv-a-unstyle-list lrv-u-flex lrv-u-height-100p lrv-u-flex-direction-column@mobile-max')
-songs = []
-#singers = []
-for hit in hits:
-    songs.append(hit.find('h3').text.strip())
-    #singers.append(hit.find('span').text.strip())
-
+hits = soup.find_all(name='ul',
+                     class_='lrv-a-unstyle-list lrv-u-flex lrv-u-height-100p lrv-u-flex-direction-column@mobile-max')
+songs = [hit.find('h3').getText().strip() for hit in hits]
