075f449ee63915df68355e2a4b7bef2b6c961736
alex <a.n.chasovskoy@gmail.com>
2023-12-26 12:39:32 +0100

    Day 48 - Selenium - initial webdriver connection

---
+++ Day 48/main.py
@@ -0,0 +1,7 @@
+# בס״ד
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.amazon.com')
