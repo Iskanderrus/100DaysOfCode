31c7b47836fc56fe662f7374d6c685a6847ca7b8
Alexander Chasovskoy <a.n.chasovskoy@gmail.com>
2025-01-24 20:44:39 +0100

    upgraded version for multy user support

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,4 +1,3 @@
-# בס״ד
 import datetime
 import json
 import random
@@ -14,115 +13,114 @@ FONT_NAME = 'TkMenuFont'
 DATA_FILE = Path('../../../Documents/password_manager_log.json')
 
 # default values for the password
-lower_case_number = 10
-upper_case_number = 8
-digits_number = 5
-symbols_number = 5
+DEFAULT_LOWER_CASE_NUMBER = 10
+DEFAULT_UPPER_CASE_NUMBER = 8
+DEFAULT_DIGITS_NUMBER = 5
+DEFAULT_SYMBOLS_NUMBER = 5
 
 
 def search_button_func():
+    """Searches for a password based on website and username combination."""
     try:
         with open(DATA_FILE, 'r') as data_file:
             data = json.load(data_file)
     except FileNotFoundError:
-        messagebox.showinfo("File does not exists",
-                            f"Please make your first entry and save it.")
-
+        messagebox.showinfo("File does not exist",
+                            "Please make your first entry and save it.")
     else:
-        website = website_entry.get()
-        try:
-            username = data[website]['username']
-            password = data[website]['password']
-        except KeyError:
-            messagebox.showinfo("Entry does not exists",
-                                f"This website is not found in your database")
-        else:
-            pyperclip.copy(password)
-            messagebox.showinfo("Entry exists",
-                                f"Saved username: {username}\n"
-                                f"Saved password: {password}\n\n"
-                                f"This password is copied to clipboard.")
+        website = website_entry.get().strip()
+        username = email_entry.get().strip()
+        found = False
+
+        for key, value in data.items():
+            if key == website:
+                if value.get("username") == username:
+                    password = value.get("password")
+                    pyperclip.copy(password)
+                    messagebox.showinfo("Entry exists",
+                                        f"Saved username: {username}\n"
+                                        f"Saved password: {password}\n\n"
+                                        f"This password is copied to clipboard.")
+                    found = True
+                    break
+
+        if not found:
+            messagebox.showinfo("Entry does not exist",
+                                "This website and username combination was not found in your database")
 
 
 def exit_button():
-    """
-    Function to exit from the app saving the password if generated to ensure that the password is not get lost
-    :return:
-    """
+    """Exits the app, saving the password if generated to ensure that the password is not lost."""
     if password_entry.get():
         adding_password()
-
     sys.exit()
 
 
 def special_requirements():
-    """
-    Function to adjust the default password settings according to user requirements.
-    User is prompted for each symbol group
-    :return:
-    """
-    global lower_case_number, upper_case_number, digits_number, symbols_number
-    lower_case_number = int(simpledialog.askstring(
-        title="Number required!",
-        prompt="How many lowercase characters are required?"))
+    """Adjusts password settings according to user requirements."""
+    global DEFAULT_LOWER_CASE_NUMBER, DEFAULT_UPPER_CASE_NUMBER, DEFAULT_DIGITS_NUMBER, DEFAULT_SYMBOLS_NUMBER
+    try:
+        DEFAULT_LOWER_CASE_NUMBER = int(simpledialog.askstring(
+            title="Number required!",
+            prompt="How many lowercase characters are required?"))
 
-    upper_case_number = int(simpledialog.askstring(
-        title="Number required!",
-        prompt="How many uppercase characters are required?"))
+        DEFAULT_UPPER_CASE_NUMBER = int(simpledialog.askstring(
+            title="Number required!",
+            prompt="How many uppercase characters are required?"))
 
-    digits_number = int(simpledialog.askstring(
-        title="Number required!",
-        prompt="How many digits are required?"))
+        DEFAULT_DIGITS_NUMBER = int(simpledialog.askstring(
+            title="Number required!",
+            prompt="How many digits are required?"))
 
-    symbols_number = int(simpledialog.askstring(
-        title="Number required!",
-        prompt="How many symbols are required?"))
+        DEFAULT_SYMBOLS_NUMBER = int(simpledialog.askstring(
+            title="Number required!",
+            prompt="How many symbols are required?"))
+    except (ValueError, TypeError):
+        messagebox.showinfo("Error!", "Please enter valid numbers for the password length requirements")
 
 
-def check_credentials():
-    """ Function to check if user entered website or username/email"""
-    if len(website_entry.get()) <= 4 or not email_entry.get():
+def check_credentials(website, username):
+    """Checks if user entered valid website and username/email."""
+    if len(website) <= 4 or not username or "@" not in username:
         # if one of the values is missing message popup
         messagebox.showinfo("Missing data", "Please check credentials")
+        return False
     else:
         return True
 
 
-def create_password():
-    """
-    Main function to generate password.
-    :return:
-    """
-    global lower_case_number, upper_case_number, digits_number, symbols_number
+def create_password(lower_case_number, upper_case_number, digits_number, symbols_number):
+    """Main function to generate password."""
     # empty the field in case user wants to regenerate the password
     password_entry.delete(0, 'end')
 
-    if check_credentials():
+    if check_credentials(website_entry.get(), email_entry.get()):
         try:
             new_password = (
                     random.sample(string.ascii_lowercase, k=lower_case_number) +
                     random.sample(string.ascii_uppercase, k=upper_case_number) +
                     random.sample(string.digits, k=digits_number) +
                     random.sample(string.punctuation, k=symbols_number))
-        except (TypeError, ValueError):
-            sys.exit()
-        else:
+
             random.shuffle(new_password)
             random.shuffle(new_password)
             password_str = ''.join(new_password)
-            # insert the generated password into the entry field
             password_entry.insert(0, password_str)
+        except (TypeError, ValueError):
+            messagebox.showinfo("Error",
+                                "Please set the password length requirements correctly using requirements button")
+
+
+def generate_password_button_func():
+    """Helper function for the generation button to create the password."""
+    global DEFAULT_LOWER_CASE_NUMBER, DEFAULT_UPPER_CASE_NUMBER, DEFAULT_DIGITS_NUMBER, DEFAULT_SYMBOLS_NUMBER
+    create_password(DEFAULT_LOWER_CASE_NUMBER, DEFAULT_UPPER_CASE_NUMBER, DEFAULT_DIGITS_NUMBER, DEFAULT_SYMBOLS_NUMBER)
 
 
 def adding_password():
-    """
-    Function aiming two goals:
-    1. Add the new website, user data and password to the file for storing
-    2. Copying the newly generated password to clipboard for immediate use after saving
-    :return:
-    """
-    website = website_entry.get()
-    email = email_entry.get()
+    """Adds the new website, user data, and password to the file for storing."""
+    website = website_entry.get().strip()
+    email = email_entry.get().strip()
     password = password_entry.get()
     date = datetime.datetime.now()
     current_date = date.strftime('%d.%m.%Y')
@@ -134,7 +132,7 @@ def adding_password():
             "logged_date": current_date
         }
     }
-    if check_credentials():
+    if check_credentials(website, email):
         try:
             with open(DATA_FILE, 'r') as data_file:
                 data = json.load(data_file)
@@ -142,15 +140,24 @@ def adding_password():
             with open(DATA_FILE, 'w') as data_file:
                 json.dump(new_entry, data_file, indent=4)
         else:
-            data.update(new_entry)
 
-            with open(DATA_FILE, 'w') as data_file:
-                json.dump(data, data_file, indent=4)
-        finally:
-            pyperclip.copy(password_entry.get())
-            messagebox.showinfo("Success", "Saved and copied to clipboard")
-            password_entry.delete(0, 'end')
-            website_entry.delete(4, 'end')
+            if website in data:  # If the website entry exists
+                data[website]["username"] = email
+                data[website]["password"] = password
+                data[website]["logged_date"] = current_date  # update the entry
+            else:
+                data.update(new_entry)  # or add a new entry
+
+        with open(DATA_FILE, 'w') as data_file:
+            json.dump(data, data_file, indent=4)
+
+        pyperclip.copy(password_entry.get())
+        messagebox.showinfo("Success", "Saved and copied to clipboard")
+        password_entry.delete(0, 'end')
+        website_entry.delete(0, 'end')
+        email_entry.delete(0, 'end')
+        website_entry.insert(0, 'www.')
+        email_entry.insert(0, '@')
 
 
 # create main window
@@ -211,7 +218,7 @@ generate_button = Button(
     activeforeground='#F9E3E2',
     highlightthickness=0,
     text='Generate Password',
-    command=create_password,
+    command=generate_password_button_func,
 )
 generate_button.grid(row=4, column=2)
 
